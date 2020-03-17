1e1484e228c3636536ea18b3426197ed11f4228d
BrainStone <yannick@tedworld.de>
2020-03-17 01:49:14 +0100

    Added test cases

---
+++ src/test/java/team/aura_dev/mersenne_benchmark/MersenneNumberGeneratorTest.java
@@ -0,0 +1,34 @@
+package team.aura_dev.mersenne_benchmark;
+
+import static org.junit.Assert.assertEquals;
+
+import java.math.BigInteger;
+import java.util.function.Function;
+import org.junit.Test;
+
+public class MersenneNumberGeneratorTest {
+  private static final int ITERATIONS = 1000;
+
+  private static final BigInteger ONE = BigInteger.ONE;
+
+  @Test
+  public void shiftLeft_subTest() {
+    assertCalculatesCorrectly(ITERATIONS, MersenneNumberGenerator::shiftLeft_sub);
+  }
+
+  @Test
+  public void setBit_subTest() {
+    assertCalculatesCorrectly(ITERATIONS, MersenneNumberGenerator::setBit_sub);
+  }
+
+  private static void assertCalculatesCorrectly(
+      int iterations, Function<Integer, BigInteger> generator) {
+    BigInteger num = ONE;
+
+    for (int i = 1; i < iterations; ++i) {
+      assertEquals(num, generator.apply(i));
+
+      num = num.shiftLeft(1).add(ONE);
+    }
+  }
+}
